8176beff67482e8e3698b859862f47124ba70595
Appecherla Nikhitha <nikhithar580@gmail.com>
2026-05-17 12:35:43 +0530

    Uncomment API key assignment in llm_engine.py

--- llm_engine.py
+++ llm_engine.py
@@ -10,7 +10,7 @@ from google.api_core.exceptions import ResourceExhausted
 # =========================
 load_dotenv()
 client = genai.Client(
-    # api_key = os.getenv("GEMINI_API_KEY")
+   api_key = os.getenv("GEMINI_API_KEY")
 )
 
 MODEL_NAME = "gemini-2.5-flash"
